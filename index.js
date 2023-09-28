f82cdc91b43e99862fa6f0e26bebd1ad301519a3
caleb <mwanziacaleb90@gmail.com>
2023-09-28 03:10:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// code your solution
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    
+    if (winningRecord) {
+      return winningRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  const winningYear = superbowlWin(record);
